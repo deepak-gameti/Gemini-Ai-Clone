1cad181be63e0823ff1e94edcf41174704bebf1a
deepak-gameti <159270881+deepak-gameti@users.noreply.github.com>
2024-06-20 20:17:47 +0530

    upload code

---
+++ src/config/gemini.js
@@ -0,0 +1,37 @@
+// const api = 'AIzaSyAl-khtxx4Q3hnlUhdMDYYd_g_WbIj5CN8'
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = import.meta.env.VITE_GEMINI_AI_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.0-pro",
+  });
+  
+  const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    topK: 0,
+    maxOutputTokens: 2048,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  }
+  
+  export default run;
